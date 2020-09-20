6333de869739f1dbe7d41478f02d35a725b543d7
Miles Rausch <miles@milesrausch.com>
2020-09-19 21:16:06 -0500

    Add AppIcon stories

---
+++ src/stories/AppIcon.stories.js
@@ -0,0 +1,55 @@
+import { argsToAttributes } from './helper.js';
+
+import '../components/AppIcon.js';
+
+export default {
+  title: 'App Components/AppIcon',
+  component: 'app-icon',
+  argTypes: {
+    name: {
+      description: 'Name of the icon',
+    },
+  },
+};
+
+const Template = args => `<app-icon ${argsToAttributes(args)}></app-icon>`;
+
+export const ArrowCircleDown = Template.bind({});
+ArrowCircleDown.args = {
+  name: 'arrow-circle-down',
+};
+
+export const ArrowCircleLeft = Template.bind({});
+ArrowCircleLeft.args = {
+  name: 'arrow-circle-left',
+};
+
+export const ArrowCircleRight = Template.bind({});
+ArrowCircleRight.args = {
+  name: 'arrow-circle-right',
+};
+
+export const ArrowCircleUp = Template.bind({});
+ArrowCircleUp.args = {
+  name: 'arrow-circle-up',
+};
+
+export const Check = Template.bind({});
+Check.args = {
+  name: 'check',
+};
+
+export const CheckCircle = Template.bind({});
+CheckCircle.args = {
+  name: 'check-circle',
+};
+
+export const MinusCircle = Template.bind({});
+MinusCircle.args = {
+  name: 'minus-circle',
+};
+
+export const PlusCircle = Template.bind({});
+PlusCircle.args = {
+  name: 'plus-circle',
+};
