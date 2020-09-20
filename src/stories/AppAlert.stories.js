7d5f6637200f111e755c18f13c713d2901adf6eb
Miles Rausch <miles@milesrausch.com>
2020-09-19 21:31:39 -0500

    Add styled versions of AppAlert stories

--- src/stories/AppAlert.stories.js
+++ src/stories/AppAlert.stories.js
@@ -45,9 +45,21 @@ InfoAlert.args = {
   hide: false,
 };
 
+export const StyledInfoAlert = Template.bind({});
+StyledInfoAlert.args = {
+  ...InfoAlert.args,
+  style: '--info-color: navy; --background-color: lightblue;',
+};
+
 export const SuccessAlert = Template.bind({});
 SuccessAlert.args = {
   level: 'success',
+};
+
+export const StyledSuccessAlert = Template.bind({});
+StyledSuccessAlert.args = {
+  ...SuccessAlert.args,
+  style: '--success-color: darkgreen; --background-color: palegreen;',
 };
 
 export const FailureAlert = Template.bind({});
@@ -55,7 +67,19 @@ FailureAlert.args = {
   level: 'failure',
 };
 
+export const StyledFailureAlert = Template.bind({});
+StyledFailureAlert.args = {
+  ...FailureAlert.args,
+  style: '--failure-color: darkred; --background-color: lightcoral;',
+};
+
 export const HiddenAlert = Template.bind({});
 HiddenAlert.args = {
   hide: true,
+};
+
+export const StyledCustomAlert = Template.bind({});
+StyledCustomAlert.args = {
+  level: 'ludicrious',
+  style: '--primary-color: gold; --background-color: black;',
 };
